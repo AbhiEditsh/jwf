951214db8ec76f6119abbc71be04b505842c6bc5
AbhiEditsh <abhishekvaghasiya58@gmail.com>
2024-12-10 17:17:01 +0530

    remove image magnify

--- src/Home/MainSlider.jsx
+++ src/Home/MainSlider.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Carousel } from "antd";
+import Slider from "react-slick";
 import { useMediaQuery } from "@mui/material";
 
 const MainSlider = () => {
@@ -13,20 +13,21 @@ const MainSlider = () => {
     objectFit: "cover",
   };
 
-  const handleBeforeChange = (from, to) => {
-    setCurrentSlide(to); // Update the current slide index
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    beforeChange: (oldIndex, newIndex) => setCurrentSlide(newIndex),
   };
 
   return (
-    <Carousel
-      autoplay
-      effect="fade"
-      autoplaySpeed={5000}
-      dotPosition="right"
-      dots={false}
-      easing="ease-in-out"
-      beforeChange={handleBeforeChange}
-    >
+    <Slider {...settings}>
+      {/* Slide 1 */}
       <div className="slider-item">
         <img
           src="https://i.postimg.cc/T37k9h4p/Untitled-design-1.jpg"
@@ -36,12 +37,13 @@ const MainSlider = () => {
         <div
           className={`slider-text ${
             currentSlide === 0 ? "animate-active" : ""
-          }`} 
+          }`}
         >
           <h1>Luxury Redefined in Every Sparkle</h1>
           <p>Adorn Yourself with the Finest Jewelry, Crafted to Perfection</p>
         </div>
       </div>
+      {/* Slide 2 */}
       <div className="slider-item">
         <img
           src="https://i.postimg.cc/15c6PT0S/banner-img-4-orig.jpg"
@@ -59,7 +61,7 @@ const MainSlider = () => {
           </p>
         </div>
       </div>
-    </Carousel>
+    </Slider>
   );
 };
 
